8cca7eb41f89f7b66180ea826d4c0392d82c2276
antoniozandate <antonio.zandate@metricamovil.com>
2021-11-28 19:31:53 -0600

    update: restaurant model

--- models/user.model.js
+++ models/user.model.js
@@ -1,15 +1,29 @@
 const mongoose = require('mongoose')
 
+const RestaurantInfoSchema = new mongoose.Schema({
+  about: { type: String, maxlength: 1000 },
+  address: { type: String },
+  name: { type: String },
+  phone: { type: String, unique: true },
+  emailContact: { type: String, unique: true }
+})
+
 const UserSchema = new mongoose.Schema(
-	{
-		username: { type: String, unique: true, required: true, select: false },
-		email: { type: String, unique: true, required: true },
-		password: { type: String, required: true, select: false },
-		fullName: { type: String, required: true },
-		birthday: { type: Date, required: false },
-		isRestaurant: { type: Boolean, required: true },
-	},
-	{ timestamps: true, _id: 'id' }
+  {
+    username: { type: String, unique: true, required: true, select: false },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true, select: false },
+    fullName: { type: String, required: true },
+    birthday: { type: Date, required: false },
+    isRestaurant: { type: Boolean, required: true },
+    restaurantInfo: {
+      type: RestaurantInfoSchema,
+      required: function () {
+        return this.isRestaurant
+      }
+    }
+  },
+  { timestamps: true, _id: 'id' }
 )
 
 module.exports = mongoose.model('Users', UserSchema)
