a7a1d7fd4af74a08869de00ee0e56663c4544d4a
antoniozandate <antonio.zandate@metricamovil.com>
2021-11-28 19:33:32 -0600

    added: route to register restaurant

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const UserController = require('../controllers/user.controller')
+const RestaurantController = require('../controllers/restaurant.controller')
 
 router.post('/register', UserController.register)
+router.post('/register/restaurant', RestaurantController.register)
 router.post('/login', UserController.login)
 router.put('/account/info', UserController.updateUserInfo)
 
